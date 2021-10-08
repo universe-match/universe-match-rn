48f9a85007659a942137150f67c7e4856e240cdf
raeyoung <raeyoung233@gmail.com>
2021-10-08 21:21:03 +0900

    그룹화면 플러스 버튼 위치 조정

--- src/screens/groups/components/AddGroupButton.tsx
+++ src/screens/groups/components/AddGroupButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, Image, TouchableOpacity} from 'react-native';
-import {colors} from '../../../constants/Index';
+import {getHeight, getWidth} from '../../../constants/Index';
 import plus from '../../../assets/images/common/plus.png';
 
 const AddGroupButton = ({navigation}: any) => {
@@ -20,20 +20,15 @@ const AddGroupButton = ({navigation}: any) => {
 const styles = StyleSheet.create({
   touchableOpacityStyle: {
     position: 'absolute',
-    width: 75,
-    height: 75,
     alignItems: 'center',
     justifyContent: 'center',
-    right: 20,
-    bottom: 50,
-    backgroundColor: colors.green,
-    borderRadius: 50,
+    right: getWidth(70),
+    top: getWidth(1045),
   },
   floatingButtonStyle: {
     resizeMode: 'contain',
-    color: 'red',
-    width: 50,
-    height: 50,
+    width: getWidth(100),
+    height: getHeight(100),
   },
 });
 
