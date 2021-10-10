a32757ef5f6019aada0e67cbc05a8d1723b69405
raeyoung <raeyoung233@gmail.com>
2021-10-10 23:14:52 +0900

    하단 tab 이미지 수정
    active 에 따라 이미지 변경

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Image} from 'react-native';
+
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {GroupScreen, ChatScreen, FriendScreen, Screen4} from './Index';
 import group from '../assets/images/bottomTab/group.png';
+import group_active from '../assets/images/bottomTab/group_active.png';
 import chat from '../assets/images/bottomTab/chat.png';
+import chat_active from '../assets/images/bottomTab/chat_active.png';
 import friends from '../assets/images/bottomTab/friends.png';
+import friends_active from '../assets/images/bottomTab/friends_active.png';
 import myInfo from '../assets/images/bottomTab/myInfo.png';
+import myInfo_active from '../assets/images/bottomTab/myInfo_active.png';
 import {getHeight, colors} from '../constants/Index';
 
 const Tab = createBottomTabNavigator();
@@ -15,24 +20,28 @@ const tabs = [
     name: 'GroupScreen',
     label: '그룹',
     image: group,
+    image_active: group_active,
     component: GroupScreen,
   },
   {
     name: 'ChatScreen',
     label: '채팅',
     image: chat,
+    image_active: chat_active,
     component: ChatScreen,
   },
   {
     name: 'FriendScreen',
     label: '친구',
     image: friends,
+    image_active: friends_active,
     component: FriendScreen,
   },
   {
     name: 'Screen4',
     label: '계정',
     image: myInfo,
+    image_active: myInfo_active,
     component: Screen4,
   },
 ];
@@ -45,13 +54,15 @@ const MainScreen = () => {
         inactiveTintColor: 'black',
         style: {height: getHeight(110)},
       }}>
-      {tabs.map(({name, label, image, component}) => (
+      {tabs.map(({name, label, image, image_active, component}) => (
         <Tab.Screen
           key={name}
           name={name}
           options={{
             tabBarLabel: `${label}`,
-            tabBarIcon: () => <Image source={image} />,
+            tabBarIcon: ({focused}) => {
+              return <Image source={focused ? image_active : image} />;
+            },
           }}
           component={component}
         />
