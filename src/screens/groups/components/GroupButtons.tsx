5d89098fd8c11d99d78ce681bd5fb0388c9f1258
raeyoung <raeyoung233@gmail.com>
2021-10-24 14:55:01 +0900

    그룹 화면 버튼 변경

--- src/screens/groups/components/GroupButtons.tsx
+++ src/screens/groups/components/GroupButtons.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import {StyleSheet, Image, TouchableOpacity} from 'react-native';
-import heart from '../../../assets/images/common/heart.png';
-import check from '../../../assets/images/common/check.png';
-import {getWidth} from '../../../constants/Index';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {colors, getHeight, getWidth} from '../../../constants/Index';
 
 const GroupButtons = ({checkIn}: any) => {
   return (
     <>
       <TouchableOpacity activeOpacity={0.5} style={styles.heartButton}>
-        <Image source={heart} />
+        <Text style={styles.buttonText}>찜하기</Text>
       </TouchableOpacity>
       <TouchableOpacity
         activeOpacity={0.5}
-        style={styles.checkButton}
+        style={styles.InButton}
         onPress={checkIn}>
-        <Image source={check} />
+        <Text style={styles.buttonText}>참여하기</Text>
       </TouchableOpacity>
     </>
   );
@@ -25,23 +23,28 @@ const styles = StyleSheet.create({
     position: 'absolute',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-    borderRadius: 22.6471,
-    width: getWidth(150),
-    height: getWidth(150),
-    top: getWidth(50),
-    right: getWidth(120),
+    backgroundColor: colors.green,
+    borderRadius: 10.9153,
+    width: getWidth(237),
+    height: getHeight(92),
+    top: getWidth(180),
+    left: getWidth(100),
   },
-  checkButton: {
+  InButton: {
     position: 'absolute',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-    borderRadius: 22.6471,
-    width: getWidth(150),
-    height: getWidth(150),
-    bottom: getWidth(50),
-    right: getWidth(120),
+    backgroundColor: colors.green,
+    borderRadius: 10.9153,
+    width: getWidth(237),
+    height: getHeight(92),
+    top: getWidth(180),
+    left: getWidth(380),
+  },
+  buttonText: {
+    color: colors.white,
+    textAlign: 'center',
+    fontSize: getWidth(30),
   },
 });
 
